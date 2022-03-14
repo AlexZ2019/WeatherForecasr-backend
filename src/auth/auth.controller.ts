509123a81026b7535f4833d538dc7128183901e6
Alex <alex@konstankino.com>
2022-03-14 18:52:06 +0200

    added test controller, added .env, installed nestjs config

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import {Body, Controller, Post} from '@nestjs/common';
+import {AuthDto} from "../dto/auth.dto";
+
+@Controller('auth')
+export class AuthController {
+
+    @Post()
+    auth(@Body() authDto: AuthDto): string {
+        return "success"
+    }
+}
