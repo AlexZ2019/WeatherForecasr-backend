bd7509add743a59210b84cae53f95f53c3aa6054
Alex <alex@konstankino.com>
2022-03-28 19:07:38 +0300

    create first hook-form with request to sign in

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = await app.get(ConfigService)
   const port = config.get<number>("API_PORT")
+  app.enableCors();
   await app.listen(port);
 }
 bootstrap();
