8ad8c9250574d552612178cb1fc731f7015144c0
Alex <alex@konstankino.com>
2022-04-20 23:56:14 +0300

    clean up

--- src/whetherForecast/entities/city.ts
+++ src/whetherForecast/entities/city.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { BaseEntity } from '../../common/entities/baseEntity';
 
 @Entity()
--- src/whetherForecast/weatherForecastResolver.ts
+++ src/whetherForecast/weatherForecastResolver.ts
@@ -28,23 +28,6 @@ export class WeatherForecastResolver {
   @Query(() => [CitiesModel])
   @UseGuards(GqlAuthGuard)
   async getCityWeatherForecast(@Args() cityArgs: CityArgs) {
-    const data = [
-      {
-        name: 'Vinnytsia',
-        lat: 49.2320162,
-        lon: 28.467975,
-        country: 'UA',
-        state: 'Vinnytsia Oblast'
-      },
-      {
-        name: 'Vinnytsia',
-        lat: 48.425616000000005,
-        lon: 35.13497630605263,
-        country: 'UA',
-        state: 'Dnipropetrovsk Oblast'
-      }
-    ];
-
     return this.weatherForecastService.findCity(cityArgs);
   }
 }
